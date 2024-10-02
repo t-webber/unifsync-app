fbf4f8146e1812447a5f4d5ae9e7d3ca5693e8f6
Tom Webber <t.c.w.webber@gmail.com>
2024-10-02 11:44:20 +0200

    ui: theme & tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const store = reactive({
+    activated: true,
+});
+</script>
 
 <template>
-    <div>Hello World</div>
+    <div class="flex h-screen">
+        <div class="p-4 bg-muted space-y-4 h-full">
+            <button @click="store.activated = !store.activated" class="w-full">
+                <v-icon name="hi-menu" />
+            </button>
+            <div v-if="store.activated">
+                <ul>
+                    <li><a>Hello World4</a></li>
+                    <li><a>Hello World4</a></li>
+                    <li><a>Hello World4</a></li>
+                    <li><a>Hello World4</a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <div>
+                <div v-if="store.activated">Edit</div>
+                <div v-else>Save</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
