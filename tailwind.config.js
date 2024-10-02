233262c9580792ce7a124ff3ed3c249c47caf18f
Tom Webber <t.c.w.webber@gmail.com>
2024-10-02 18:07:24 +0200

    add: prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", 
-    "./src/**.{vue, ts, js}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        bg: 'var(--bg)', 
-        fg: 'var(--fg)', 
-        primary: 'var(--primary)', 
-        muted: 'var(--muted)'
-      }, 
+    content: ["./index.html", "./src/**.{vue, ts, js}"],
+    theme: {
+        extend: {
+            colors: {
+                bg: "var(--bg)",
+                fg: "var(--fg)",
+                primary: "var(--primary)",
+                muted: "var(--muted)",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
