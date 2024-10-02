20aedf46519267dbb12383dcb0e55a375988d765
Tom Webber <t.c.w.webber@gmail.com>
2024-10-02 18:07:43 +0200

    add: communication tauri/frontend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,9 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let res = format!("Hello, {name}! You've been greeted from Rust!");
+    println!("{res}");
+    res
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
