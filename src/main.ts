33b04ef9b57d56ec1773a075053d21546b7b1109
Tom Webber <t.c.w.webber@gmail.com>
2024-10-02 20:28:57 +0200

    add: router

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./main.css";
-import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { HiMenu } from "oh-vue-icons/icons";
-
 declare global {
     interface Window {
         __TAURI__: {
@@ -14,8 +8,39 @@ declare global {
     }
 }
 
-addIcons(HiMenu);
+// Create App from vue files
 
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./main.css";
 const app = createApp(App);
+
+// Add icons
+
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { HiMenu } from "oh-vue-icons/icons";
+addIcons(HiMenu);
 app.component("v-icon", OhVueIcon);
+
+// Add router
+
+import { createMemoryHistory, createRouter } from "vue-router";
+
+import Home from "./home.vue";
+import Note from "./note.vue";
+
+const routes = [
+    { path: "/", component: Home },
+    { path: "/note/:id", component: Note },
+];
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+});
+
+app.use(router);
+
+// Mount the app on html
+
 app.mount("#app");
